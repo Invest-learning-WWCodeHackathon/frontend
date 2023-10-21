7feea89da8163e1264348dee03eaa9b091420a73
pradnyabhukan <bhukanpradnya5@gmail.com>
2023-10-20 20:09:42 -0400

    hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,7 @@
 import {
   Container, Stack, Flex, Box, Heading, Text, Button, Image, Icon, IconButton, createIcon, IconProps, useColorModeValue,
 } from '@chakra-ui/react'
+import HeroImage from "../assests/Hero.jpeg"
 
 export default function Hero() {
   return (
@@ -78,7 +79,7 @@ export default function Hero() {
             boxShadow={'2xl'}
             width={'full'}
             overflow={'hidden'}>
-            <IconButton
+            {/* <IconButton
               aria-label={'Play Button'}
               variant={'ghost'}
               _hover={{ bg: 'transparent' }}
@@ -89,15 +90,15 @@ export default function Hero() {
               left={'50%'}
               top={'50%'}
               transform={'translateX(-50%) translateY(-50%)'}
-            />
+            /> */}
             <Image
               alt={'Hero Image'}
               fit={'cover'}
               align={'center'}
               w={'100%'}
               h={'100%'}
-              src={
-                'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+              src={ HeroImage
+                // 'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
               }
             />
           </Box>
